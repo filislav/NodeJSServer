7a957f69dbbbb6723f4f2bfb966cd989947327a6
filislav <filislav43@gmail.com>
2021-07-12 16:23:20 +0400

    ejs templates

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-let http = require('http');
-let threads = require('./threads');
-let fs = require('fs');
+let express = require('express');
 
-const PORT = 3000;
-let server = http.createServer((request,responce)=>{
-    console.log(`URL стрвницы: ${request.url}`) //отслеживаем в консоли URL
-    if(request.url==="/index"||request.url==="/"){
-        responce.writeHead(200,{"Content-Type":"text/html; charset=utf-8"});
-        fs.createReadStream(__dirname + "/index.html").pipe(responce);
-    }else if(request.url==="/about"){
-        responce.writeHead(200,{"Content-Type":"text/html; charset=utf-8"});
-        fs.createReadStream(__dirname + "/about.html").pipe(responce);
-    }else if(request.url==="/user"){
-        responce.writeHead(200,{"Content-Type":"application/json; charset=utf-8"});
-        let user = {name:'slava',age:38,gender:'man'};
-        responce.end(JSON.stringify(user));
-    }else{
-        responce.writeHead(404,{"Content-Type":"text/html; charset=utf-8"});
-        fs.createReadStream(__dirname + "/error.html").pipe(responce);
-    }
+let app = express();
+app.set('view engine','ejs');
+app.get('/',(request,response)=>{
+    response.sendFile(__dirname+'/index.html');
 });
-server.listen(PORT,"127.0.0.1");
-console.log(`Сервер на порту: ${PORT}`);
+app.get('/about',(request,responce)=>{
+    responce.sendFile(__dirname+'/about.html');
+})
+app.get('/news/:id',(request,responce)=>{
+    let obj= {title:'Новость',id:4,paragraphs:[1,2,3,4]};
+    responce.render('news',{newsId:request.params.id,obj:obj});
+})
+app.listen(3000);
